79be8fee626e141b050828a4f7b93fc6b4cc678b
Ginn <ginnlaiho@gmail.com>
2021-05-31 15:03:33 -0500

    Add delete button to books

--- js/main.js
+++ js/main.js
@@ -14,15 +14,26 @@ function addBookToLibrary() {
   const bookContainer = document.querySelector('#book-display');
 
   bookContainer.innerHTML +=`
-
   <div class="card border-success mb-3 me-3 text-center" style="width: 18rem;">
     <div class="card-header text-primary">${author}</div>
     <div class="card-body text-success">
       <h5 class="card-title">${title}</h5>
       <p class="card-text">${pages}</p>
       <p class="card-text">${(bookRead) ? 'Book read' : 'Not read yet'}</p>
+      <button type="button" class="btn btn-danger me-3" id="deleteBook">Delete Book</button>
     </div>
   </div>`
 
   document.querySelector('#add-book').reset();
 }
+
+// Delete Books
+
+const books = document.querySelector('#book-display');
+
+books.addEventListener('click', function (e) {
+    if (e.target.className.indexOf('btn-danger') != -1) {
+        const card = e.target.parentElement.parentElement;
+        books.removeChild(card);
+    }
+});
