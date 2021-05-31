be529137619e047650aa55ba83a294c65dda9ff4
Alexis Varela <alexisbec13@hotmail.com>
2021-05-31 15:33:11 -0500

    add myLibrary array and constructor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
 const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit', function (e) {
@@ -11,16 +20,23 @@ function addBookToLibrary() {
   const pages = addForm.querySelector('#bookPages').value;
   const bookRead = addForm.querySelector('#bookRead').checked;
 
+  const newBook = new Book(author, title, pages, bookRead);
+
+  myLibrary.push(newBook);
+
   const bookContainer = document.querySelector('#book-display');
+
+  let book = myLibrary[myLibrary.length - 1];
 
   bookContainer.innerHTML +=`
   <div class="card border-success mb-3 me-3 text-center" style="width: 18rem;">
-    <div class="card-header text-primary">${author}</div>
+    <div class="card-header text-primary">${book.author}</div>
     <div class="card-body text-success">
-      <h5 class="card-title">${title}</h5>
-      <p class="card-text">${pages}</p>
-      <p class="card-text">${(bookRead) ? 'Book read' : 'Not read yet'}</p>
+      <h5 class="card-title">${book.title}</h5>
+      <p class="card-text">${book.pages}</p>
+      <p class="card-text">${(book.read) ? 'Book read' : 'Not read yet'}</p>
       <button type="button" class="btn btn-danger me-3" id="deleteBook">Delete Book</button>
+      <button type="button" class="btn btn-info me-3" id="readBook">Read Book</button>
     </div>
   </div>`
 
