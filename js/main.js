3ba2f1e4f74ffb3bcb911f55cd0e7874c43e3e0b
Ginn <ginnlaiho@gmail.com>
2021-05-31 13:03:55 -0500

    Add book display to project

--- js/main.js
+++ js/main.js
@@ -23,24 +23,24 @@ function addBookToLibrary() {
   const newBook = new Book(author, title, pages, bookRead);
   myLibrary.push(newBook);
 
-  const container = document.querySelector('#book-display');
-  myLibrary[myLibrary.length - 1] =
-    container.innerHTML += `
-    <div class="d-flex justify-content-around">
-      <div class="card text-white bg-dark mb-3" style="width: 18rem;">
-        <div class="card-header">${book.author}</div>
-        <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.pages}</p>
-        </div>
-      </div>
-    </div>`
-  );
+  book = myLibrary[myLibrary.length - 1];
+
+  const bookContainer = document.querySelector('#book-display');
+
+  bookContainer.innerHTML +=`
+
+  <div class="card border-success mb-3 me-3 text-center" style="width: 18rem;">
+    <div class="card-header text-primary">${book.author}</div>
+    <div class="card-body text-success">
+      <h5 class="card-title">${book.title}</h5>
+      <p class="card-text">${book.pages}</p>
+      <p class="card-text">${(book.checkIfRead) ? 'Book read' : 'Not read yet'}</p>
+    </div>
+  </div>`
+
   console.log(myLibrary);
 }
 
 function resetForm() {
   document.querySelector('#add-book').reset();
 }
-
-
