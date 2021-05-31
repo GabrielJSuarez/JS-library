b7a7c5c9ae308f1bf7474f476d863ddf13d03094
Ginn <ginnlaiho@gmail.com>
2021-05-31 14:17:52 -0500

    Refactor function to not use objects/arrays

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-let myLibrary = [];
-
-function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.checkIfRead = read;
-}
-
 const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit', function (e) {
@@ -20,27 +11,18 @@ function addBookToLibrary() {
   const pages = addForm.querySelector('#bookPages').value;
   const bookRead = addForm.querySelector('#bookRead').checked;
 
-  const newBook = new Book(author, title, pages, bookRead);
-  myLibrary.push(newBook);
-
-  book = myLibrary[myLibrary.length - 1];
-
   const bookContainer = document.querySelector('#book-display');
 
   bookContainer.innerHTML +=`
 
   <div class="card border-success mb-3 me-3 text-center" style="width: 18rem;">
-    <div class="card-header text-primary">${book.author}</div>
+    <div class="card-header text-primary">${author}</div>
     <div class="card-body text-success">
-      <h5 class="card-title">${book.title}</h5>
-      <p class="card-text">${book.pages}</p>
-      <p class="card-text">${(book.checkIfRead) ? 'Book read' : 'Not read yet'}</p>
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">${pages}</p>
+      <p class="card-text">${(bookRead) ? 'Book read' : 'Not read yet'}</p>
     </div>
   </div>`
 
-  console.log(myLibrary);
-}
-
-function resetForm() {
   document.querySelector('#add-book').reset();
 }
