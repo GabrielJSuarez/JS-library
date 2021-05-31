de58180a10a2d971a3d55036cb12d80272044b70
Alexis Varela <alexisbec13@hotmail.com>
2021-05-31 12:43:18 -0500

    add card container for myLibrary array

--- js/main.js
+++ js/main.js
@@ -22,9 +22,25 @@ function addBookToLibrary() {
 
   const newBook = new Book(author, title, pages, bookRead);
   myLibrary.push(newBook);
+
+  const container = document.querySelector('#book-display');
+  myLibrary[myLibrary.length - 1] =
+    container.innerHTML += `
+    <div class="d-flex justify-content-around">
+      <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+        <div class="card-header">${book.author}</div>
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">${book.pages}</p>
+        </div>
+      </div>
+    </div>`
+  );
   console.log(myLibrary);
 }
 
 function resetForm() {
   document.querySelector('#add-book').reset();
 }
+
+
