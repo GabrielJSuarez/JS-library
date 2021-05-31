8bc83d2547073ca33d6b7c18b8691e228ec42696
Ginn <ginnlaiho@gmail.com>
2021-05-31 11:31:03 -0500

    Add form to HTML and DOM JS to add the content to the form to JS

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.checkIfRead = read;
+}
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  const author = addForm.querySelector('#bookAuthor').value;
+  const title = addForm.querySelector('#bookTitle').value;
+  const pages = addForm.querySelector('#bookPages').value;
+  const bookRead = addForm.querySelector('#bookRead').checked;
+
+  const newBook = new Book(author, title, pages, bookRead);
+  myLibrary.push(newBook);
+});
+
+console.log(myLibrary);
+function addBookToLibrary() {
+
+}
+
+
