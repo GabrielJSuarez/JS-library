77bc5ba5b35f8003075809b1d0557a05c9ba3fef
Ginn <ginnlaiho@gmail.com>
2021-05-31 19:50:00 -0500

    - Add book array with pre-set data for showing in the webpage

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Books Array
+let myLibrary = [
+  {
+    author: 'George Orwell',
+    title: '1984',
+    pages: '2000',
+    read: true
+  },
+  {
+    author: 'George R.R Martin',
+    title: 'A song of ice and fire',
+    pages: '3000',
+    read: false
+  },
+  {
+    author: 'Andrzej Sapkowski',
+    title: 'The witcher, the last wish',
+    pages: '500',
+    read: true
+  },
+  {
+    author: 'J.R.R Tolkien',
+    title: 'The Lord Of the Rings, The Return Of the King',
+    pages: '1500',
+    read: true
+  }
+];
