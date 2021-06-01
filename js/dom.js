faaef9f595a1d9392612334473d5a30602a1fa47
Ginn <ginnlaiho@gmail.com>
2021-05-31 19:53:22 -0500

    - Create new book objects from UI and adding it to the Library Array

--- js/dom.js
+++ js/dom.js
@@ -7,3 +7,17 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function (e) {
   e.preventDefault();
 });
+
+// Create new book objects from UI and adding it to the Library Array
+
+function addBookToLibrary() {
+  const author = addForm.querySelector('#bookAuthor').value;
+  const title = addForm.querySelector('#bookTitle').value;
+  const pages = addForm.querySelector('#bookPages').value;
+  const bookRead = addForm.querySelector('#bookRead').checked;
+
+  const newBook = new Book(author, title, pages, bookRead);
+
+  myLibrary.push(newBook);
+  booksList();
+}
