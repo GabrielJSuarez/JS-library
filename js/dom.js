f2e7df449a4990ce42a9a46f54583d888b84d1ff
Ginn <ginnlaiho@gmail.com>
2021-05-31 19:54:11 -0500

    - Add the book arrays to the UI

--- js/dom.js
+++ js/dom.js
@@ -21,3 +21,28 @@ function addBookToLibrary() {
   myLibrary.push(newBook);
   booksList();
 }
+
+// Add the book arrays to the UI
+
+function booksList() {
+  const bookDisplay = document.querySelector('#book-display');
+  bookDisplay.innerHTML = '';
+
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    bookDisplay.innerHTML += `
+    <div class="card border-success m-3 text-center" style="width: 23rem;">
+      <div class="card-header text-primary">${myLibrary[i].author}</div>
+      <div class="card-body text-success">
+        <h3 class="card-title">${myLibrary[i].title}</h3>
+        <p class="card-text">${myLibrary[i].pages}</p>
+        <p class="card-text">Did you read it?: <strong class="text-capitalize">${(myLibrary[i].read)}</strong></p>
+        <div class="d-flex">
+        <button type="button" class="btn btn-danger mx-3" onClick="deleteBook(${i})">Delete Book</button>
+        <button type="button" class="btn btn-info mx-3 text-light" onClick="readBook(${i})">Change Read</button>
+        </div>
+      </div>
+    </div>`
+
+    document.querySelector('#add-book').reset();
+  }
+}
